21cab6635cff9a8bfaa6757ce66d115ec4a0cd8d
KinanToutoungy <kinan.toutoungy@polymtl.ca>
2021-02-20 23:00:46 -0500

    Exercice 1 : OK

--- exercice.py
+++ exercice.py
@@ -8,7 +8,29 @@ def get_bill(name, data):
 	INDEX_QUANTITY = 1
 	INDEX_PRICE = 2
 
-	return ""
+	sous_total = 0
+
+	for i in data:
+		sous_total += i[INDEX_QUANTITY]*i[INDEX_PRICE]
+
+	taxes = sous_total*0.15
+	total = sous_total + taxes
+
+	data_bill = [
+		("SOUS TOTAL", sous_total),
+		("TAXES     ", taxes),
+		("TOTAL     ", total)
+	]
+
+	result = name
+	for db in data_bill:
+		result += "\n" + f"{db[0]} {db[1] : >10.2f} $"
+
+	#print(f"SOUS TOTAL {format(sous_total, '.2f'): >10} $")
+	#print(f"TAXES {format(taxes, '.2f') : >10} $")
+	#print(f"TOTAL {format(total, '.2f'): >10} $")
+
+	return result
 
 
 def format_number(number, num_decimal_digits):
